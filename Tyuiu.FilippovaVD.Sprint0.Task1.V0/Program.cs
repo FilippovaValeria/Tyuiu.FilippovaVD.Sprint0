0cd7867580318110fee19c3fb6ceee825f348335
FilippovaValeria <filippovavaleria34@gmail.com>
2026-09-12 16:58:32 +0500

    Add project files.

---
+++ Tyuiu.FilippovaVD.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Tyuiu.FilippovaVD.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Валерия";
+            int age = 17;
+            Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ Tyuiu.FilippovaVD.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.FilippovaVD.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.FilippovaVD.Sprint0.Task2.V0
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(DataService.GetMessage("Валерия"));
+            Console.ReadKey();
+        }
+    }
+}
