48f52caf684b4234deab67d8abcf31ac75ce3b1f
FilippovaValeria <filippovavaleria34@gmail.com>
2026-09-13 18:47:24 +0500

    Task4

---
+++ Tyuiu.FilippovaVD.Sprint0.Task4.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿namespace Tyuiu.FilippovaVD.Sprint0.Task4.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Division(int a, int b)
+        {
+            return a / b;
+        }
+
+    }
+}
